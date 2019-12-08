bc0e407ed32e36bda2fc1e9db8b39fdeb0470478
JesWu <jeffreywu1997@gmail.com>
2019-12-07 23:05:45 -0800

    Mentions WIP

--- bot.js
+++ bot.js
@@ -8,7 +8,13 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    content = msg.content;
+    var dietcoke = msg.content.toLowerCase();
+    if(dietcoke.includes("diet coke") || dietcoke.includes("dietcoke")){
+        //msg.channel.send("<@152656874427121666>")
+    }
+    getUserFromMention(msg.content);
+
+    var content = msg.content;
     if(msg.mentions.everyone){
         msg.reply("Kys");
     }else
@@ -34,3 +40,17 @@ client.on('message', msg => {
 });
 
 client.login(auth.token);
+
+function getUserFromMention(mention) {
+	if (!mention) return;
+
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+
+		console.log(client.users.get(mention));
+	}
+}
