ab765dfa1d4c2857e35c347c3d4ccecef43e45bf
JesWu <jeffreywu1997@gmail.com>
2019-12-16 12:54:54 -0800

    Cleanup commit.

--- bot.js
+++ bot.js
@@ -21,40 +21,6 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.author.bot) return;
-    if (msg.author.id == "152656874427121666"){
-        msg.react('1️⃣')
-        .then(() => msg.react('9️⃣'))
-        .then(() => msg.react('0️⃣'))
-        .then(() => msg.react('7️⃣'))
-        .catch(() => console.error('One of the emojis failed to react.'));
-    }
-    var drinks = msg.content.toLowerCase();
-    if (drinks.includes("diet coke") || drinks.includes("dietcoke")) {
-        msg.channel.send("<@152656874427121666>");
-    }
-    if (drinks.includes("milk tea") || drinks.includes("boba") || drinks.includes("coffee")) {
-        if (drinks.includes("coffee") && drinks.includes("friendship")) {
-            msg.channel.send("<@92879830365773824> ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°) ( ͡° ͜ʖ ͡°)");
-        } else {
-            msg.channel.send("<@92879830365773824>");
-        }
-    }
-    if (msg.mentions.everyone) {
-        msg.reply("Kys");
-    }
-    if (drinks.includes("no comment")) {
-        msg.channel.send("<@178759982039171073>");
-    }
-    /*
-    if(drinks.includes("mae")){
-        msg.channel.send("alex eric neil shohei");
-    }
-    */
-    if (drinks.includes("dicky luong")) {
-        msg.channel.send("shawn zack");
-    }
-
-    //getUserFromMention(msg.content);
     console.log(msg.author.username + ": " + msg.author.id);
 
     var args = msg.content.split(" ");
@@ -64,40 +30,6 @@ client.on('message', msg => {
     if (content.charAt(0) == '!') {
         content = content.slice(1);
         switch (content) {
-            case 'uwu':
-                msg.reply('owo');
-                break;
-            case 'owo':
-                msg.reply('uwu');
-                break;
-            case 'bad':
-            case 'Bad': 
-                msg.reply("Eric");
-                break;
-            case 'Eric':
-                msg.reply("Eroc > Eric.");
-                break;
-            case 'Brian':
-                msg.reply("Diet Coke.");
-                break;
-            case 'Neil':
-                msg.reply("Hey this is Neil.");
-                break;
-            case 'Peter':
-                var embed = new RichEmbed()
-                    .setTitle('Pizza bwead')
-                    .setColor(0xFF0000)
-                    .setImage('https://f4d5s4a5.stackpathcdn.com/wp-content/uploads/2018/05/Homemade_Pita_Bread_Sweet_Simple_Vegan-768x1151.jpg')
-                    .setDescription('Real picture of pizza bwead.');
-                msg.channel.send(embed);
-                break;
-            case 'Ed':
-                msg.channel.send("Ed: koreaboo who loves friendship and boba. Also known as the guy most likely to cheat on his non existent gf.");
-                break;
-            case 'Viv':
-            case 'Vivian':
-                msg.channel.send("Eh? she's aite I guess..");
-                break;
             case 'Help':
             case 'help':
                 msg.channel.send("Lmao u think this has documentation?");
@@ -108,14 +40,14 @@ client.on('message', msg => {
                 console.log(args[2]);
                 if (args[1] != null && !isNaN(args[1]) && args[2] != null && !isNaN(args[2])) {
                     var numRolls = parseInt(args[2], 10);
-                    if(numRolls > 1000){
+                    if (numRolls > 1000) {
                         msg.channel.send("Number of rolls too large.(greater than 1000)");
                         return;
                     }
                     var results = "";
                     msg.channel.send(args[2] + " rolls : ");
                     for (var i = 0; i < numRolls; i++) {
-                        if(i != 1 && i % 50 == 1){
+                        if (i != 1 && i % 50 == 1) {
                             msg.channel.send(results);
                             results = "";
                         }
@@ -171,51 +103,67 @@ client.on('message', msg => {
                 if (args[1] != null && !isNaN(args[1]) && args[2] != null && !isNaN(args[2])) {
                     var num = parseInt(args[2], 10);
                     var numDice = parseInt(args[1], 10);
-                    if(num >= 1 && num <= 6 && numDice >= 1 && numDice <= Turn.CurDice){
-                        if(curGuess.NumDice < numDice || (curGuess.NumDice == numDice && curGuess.Num < num)){
+                    if (num >= 1 && num <= 6 && numDice >= 1 && numDice <= Turn.CurDice) {
+                        if (curGuess.NumDice < numDice || (curGuess.NumDice == numDice && curGuess.Num < num)) {
                             msg.reply("Has guessed: " + numDice + " dice for " + num);
                             curGuess = {
                                 Num: num,
                                 NumDice: numDice,
                                 ID: msg.author.id
                             }
-                        }else{
+                        } else {
                             msg.reply("Your guess must be higher in number of dice or number.");
                         }
-                    }else{
+                    } else {
                         msg.reply("Invalid inputs/ dice amount too high.");
                     }
                     break;
                 }
             case 'bs':
-                if(curGuess.ID == null){
+                if (curGuess.ID == null) {
                     msg.reply("No guess currently.");
                     return;
                 }
                 msg.reply("Has called BS on <@" + curGuess.ID + ">'s guess of " + curGuess.NumDice + " dice for " + curGuess.Num);
 
                 var rollStr = Turn.CurDice + " rolls for this turn | ";
-                for(var i = 1; i <= Turn.RollFreq.length; i++){
+                for (var i = 1; i <= Turn.RollFreq.length; i++) {
                     rollStr += i + ": " + Turn.RollFreq[i - 1] + " |";
                 }
                 msg.channel.send(rollStr);
-                if(isValidGuess(curGuess)){
+                if (isValidGuess(curGuess)) {
                     msg.channel.send("<@" + curGuess.ID + ">'s bs was wrong :cry: -1 dice");
                     for (var i = 0; i < players.length; i++) {
-                        if(players[i].ID == curGuess.ID){
+                        if (players[i].ID == curGuess.ID) {
                             players[i].Dice -= 1;
                         }
                     }
-                }else{
+                } else {
                     msg.channel.send("<@" + msg.author.id + ">'s guess was wrong :cry: -1 dice");
                     for (var i = 0; i < players.length; i++) {
-                        if(players[i].ID == curGuess.ID){
+                        if (players[i].ID == curGuess.ID) {
                             players[i].Dice -= 1;
                         }
                     }
                 }
                 nextTurn(players, msg);
                 //check if win
+                if (players.length == 1) {
+                    msg.channel.send("<@" + players[0].ID + "> wins!");
+                    gameState = false;
+                    players = new Array();
+                    Turn = {
+                        CurDice: 0,
+                        Turn: 0,
+                        RollFreq: null
+                    }
+                    curGuess = {
+                        Num: 0,
+                        NumDice: 0,
+                        ID: null
+                    };
+                    msg.channel.send("Game Ended.");
+                }
                 break;
 
             case 'exit':
@@ -235,6 +183,25 @@ client.on('message', msg => {
                 break;
             case 'echo':
                 msg.channel.send(msg.content.slice(6));
+                msg.delete(0);
+                break;
+            case 'uwuify':
+                var modStr = msg.content.slice(8);
+                var newStr = "";
+                for(var i = 0; i < modStr.length; i++){
+                    if(modStr.charAt(i) == 'r' || modStr.charAt(i) == 'l'){
+                        newStr += "w";
+                    }else if(modStr.charAt(i) == 'R' || modStr.charAt(i) == 'L'){
+                        newStr += "W";
+                    }else if(modStr.charAt(i) == 'N' || modStr.charAt(i) == 'n' && isVowel(modStr.charAt(i + 1))){
+                        newStr += modStr.charAt(i);
+                        newStr += "y";
+                    }else{
+                        newStr += modStr.charAt(i);
+                    }
+                }
+                msg.channel.send(newStr);
+                msg.delete(0);
                 break;
         }
     }
@@ -242,6 +209,13 @@ client.on('message', msg => {
 });
 
 client.login(auth.token);
+
+function isVowel(char){
+    if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'){
+        return true;
+    }
+    return false;
+}
 
 function constructDice(mentions) {
     var players = new Array();
@@ -299,7 +273,7 @@ function nextTurn(players, msg) {
         ID: null
     };
     for (var j = 0; j < players.length; j++) {
-        if(players[j].Dice == 0){
+        if (players[j].Dice == 0) {
             msg.channel.send(players[j].Name + " has been eliminated.");
             players = players.splice(j, 1);
             break;
@@ -324,7 +298,7 @@ function nextTurn(players, msg) {
 
 function isValidGuess(guess) {
     console.log(guess.NumDice + " " + Turn.RollFreq[guess.Num - 1] + " " + guess.Num)
-    if(guess.NumDice > Turn.RollFreq[guess.Num - 1]){
+    if (guess.NumDice > Turn.RollFreq[guess.Num - 1]) {
         return false;
     }
     return true;
