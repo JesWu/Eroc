3d76b83a5e0fedd3df187a8884256575ce3bbd81
JesWu <jeffreywu1997@gmail.com>
2020-01-03 13:17:20 -0800

    Working on youtube integrations.

--- bot.js
+++ bot.js
@@ -3,6 +3,16 @@ const client = new Client();
 const auth = require("./auth.json");
 const dice = require("./liarsdice.js");
 const poll = require("./poll.js");
+const ytdl = require("ytdl-core");
+const ytpl = require("ytpl");
+const ytsr = require("ytsr");
+var streamOptions = {
+  seek: new Number(0),
+  volume: new Number(0.5),
+  highWaterMark: new Number(1),
+  bitrate: "auto"
+};
+var musicQueue = [];
 var interval = null;
 
 client.on("ready", () => {
@@ -10,6 +20,7 @@ client.on("ready", () => {
 });
 
 client.on("message", msg => {
+  if (!msg.guild) return;
   if (msg.author.bot) return;
   console.log(msg.author.username + ": " + msg.author.id);
 
@@ -19,13 +30,192 @@ client.on("message", msg => {
   if (args.length == 0) return;
 
   var content = args[0];
-  if (content.charAt(0) == "!") {
+  if (content.charAt(0) == "~") {
     content = content.slice(1);
     switch (content) {
+      case "join":
+        if (msg.member.voiceChannel) {
+          msg.member.voiceChannel
+            .join()
+            .then(connection => {
+              const dispatcher = connection.playFile(
+                require("path").join(__dirname, "./nggyu.mp3")
+              );
+            })
+            .catch(console.log);
+        } else {
+          msg.reply("You need to join a voice channel first!");
+        }
+        break;
+      case "clearqueue":
+        musicQueue = [];
+        msg.channel.send("Queue cleared.");
+        break;
+      case "queue":
+        if (musicQueue.length == 0) {
+          msg.reply("No music in queue.");
+        } else {
+          var infoString = "";
+          for (let i = 0; i < musicQueue.length; i++) {
+            infoString += i + 1 + ". " + musicQueue[i].info.title + "\n";
+            if(infoString.length > 1900){
+              msg.channel.send(infoString);
+              infoString = "";
+            }
+          }
+          msg.channel.send(infoString);
+        }
+        break;
+      case "play":
+        if (msg.member.voiceChannel) {
+          if (!ytdl.validateURL(args[1])) {
+            msg.reply("Enter a valid yt url.");
+            return;
+          }
+          if (!msg.member.voiceChannel.connection) {
+            msg.member.voiceChannel
+              .join()
+              .then(connection => {
+                const yt = ytdl(args[1], {
+                  filter: "audioonly",
+                  quality: "highestaudio",
+                  highWaterMark: 1 << 25
+                });
+                var dispatcher = connection.playStream(yt, streamOptions);
+                yt.on("info", info => {
+                  msg.channel.send(
+                    "Now playing: " + info.title + " :musical_note:"
+                  );
+                });
+                dispatcher.on("end", function() {
+                  dispatcher = null;
+                  console.log("End event logged.");
+                  nextSong(connection, msg);
+                });
+              })
+              .catch(console.log);
+              if(ytpl.validateURL(args[1])){
+                ytpl(args[1]).then(playListInfo => {
+                  msg.channel.send("Imported " + playListInfo.total_items + " from playlist: " + playListInfo.title);
+                  for(const item of playListInfo.items){
+                    musicQueue.push({ url: "" + item.url, info: item });
+                  }
+                  musicQueue.splice(0, 1);
+                });
+              }
+          } else {
+            if(ytpl.validateURL(args[1])){
+              console.log("Playlist Found!");
+              ytpl(args[1]).then(playListInfo => {
+                for(const item of playListInfo.items){
+                  musicQueue.push({ url: "" + item.url, info: item });
+                }
+              });
+            }else{
+              ytdl.getInfo(args[1]).then(info => {
+                musicQueue.push({ url: "" + args[1], info: info });
+              });
+            }
+            msg.reply("Song has been added to queue.");
+            return;
+          }
+        } else {
+          msg.reply("You need to join a voice channel first!");
+        }
+        break;
+      case "skip":
+        if (msg.member.voiceChannel) {
+          msg.reply("Skipped song.");
+          msg.member.voiceChannel.connection.dispatcher.end();
+        } else {
+          msg.reply("You need to join a voice channel first!s");
+        }
+        break;
+      case "pause":
+        if (msg.member.voiceChannel) {
+          const dispatcher = msg.member.voiceChannel.connection.dispatcher;
+          dispatcher.pause(); 
+          msg.reply("Song paused.");
+        } else {
+          msg.reply("You need to join a voice channel first!p");
+        }
+        break;
+      case "resume":
+        if (msg.member.voiceChannel) {
+          const dispatcher = msg.member.voiceChannel.connection.dispatcher;
+          dispatcher.resume();
+          msg.reply("Song resumed.");
+        } else {
+          msg.reply("You need to join a voice channel first!r");
+        }
+        break;
+      case "volume":
+        if (msg.member.voiceChannel) {
+          if (isNaN(args[1]) || args[1] < 0 || args[1] > 100) {
+            msg.reply("Enter a valid number (0-100)");
+            return;
+          }
+          const dispatcher = msg.member.voiceChannel.connection.dispatcher;
+          streamOptions.volume = parseInt(args[1], 10) / 10;
+          dispatcher.setVolume(args[1] / 100);
+        } else {
+          msg.reply("wtf?");
+        }
+        break;
+      case "leave":
+        if (msg.member.voiceChannel) {
+          musicQueue = [];
+          msg.member.voiceChannel.leave();
+          msg.reply("Left channel.");
+        } else {
+          msg.reply("Error: bot not in voice channel.");
+        }
+        break;
+
+      case "random":
+        const users = [];
+        msg.guild.members.forEach(member => users.push(member.user.id));
+        msg.channel.send(
+          ": <@" + users[Math.floor(Math.random() * users.length)] + ">"
+        );
+        break;
+      // case "kick":
+      //   const user = msg.mentions.users.first();
+      //   // If we have a user mentioned
+      //   if (user) {
+      //     // Now we get the member from the user
+      //     const member = msg.guild.member(user);
+      //     // If the member is in the guild
+      //     if (member) {
+      //       /**
+      //        * Kick the member
+      //        * Make sure you run this on a member, not a user!
+      //        * There are big differences between a user and a member
+      //        */
+      //       member
+      //         .kick("Optional reason that will display in the audit logs")
+      //         .then(() => {
+      //           // We let the message author know we were able to kick the person
+      //           msg.reply(`Successfully kicked ${user.tag}`);
+      //         })
+      //         .catch(err => {
+      //           // An error happened
+      //           // This is generally due to the bot not being able to kick the member,
+      //           // either due to missing permissions or role hierarchy
+      //           msg.reply("I was unable to kick the member");
+      //           // Log the error
+      //           console.error(err);
+      //         });
+      //     } else {
+      //       // The mentioned user isn't in this guild
+      //       msg.reply("That user isn't in this guild!");
+      //     }
+      //     break;
+      //   }
       case "Help":
       case "help":
         msg.channel.send(
-          "!game, !roll, !pic, !peek, !guess, !bs, !exit, !uwuify, !echo"
+          "~game, ~roll, ~poll, ~pic, ~peek, ~guess, ~bs, ~exit, ~uwuify, ~echo"
         );
         break;
       case "roll":
@@ -140,7 +330,7 @@ client.on("message", msg => {
         if (!poll.isPoll()) {
           poll.createPoll(authorID, question);
           msg.reply(
-            "Your poll has been created. Please vote using !y for yes and !n for no. !pollresults for poll results. !closepoll to close poll. Poll will close automatically in 60 seconds."
+            "Your poll has been created. Please vote using ~y for yes and ~n for no. ~pollresults for poll results. ~closepoll to close poll. Poll will close automatically in 60 seconds."
           );
           var embed = new RichEmbed()
             .setTitle(poll.getQuestion())
@@ -257,4 +447,30 @@ function isVowel(char) {
   return (
     char == "a" || char == "e" || char == "i" || char == "o" || char == "u"
   );
+}
+
+function nextSong(connection, msg) {
+  if (musicQueue.length > 0) {
+    msg.channel.send("Moving to next song..");
+    const yt = ytdl("" + musicQueue[0].url, {
+      filter: "audioonly",
+      quality: "highestaudio",
+      highWaterMark: 1 << 25
+    });
+    var dispatcher = msg.member.voiceChannel.connection.playStream(
+      yt,
+      streamOptions
+    );
+    yt.on("info", info => {
+      msg.channel.send("Now playing: " + info.title + " :musical_note:");
+    });
+    musicQueue.splice(0, 1);
+    dispatcher.on("end", function() {
+      dispatcher = null;
+      nextSong(connection, msg);
+    });
+  } else {
+    msg.member.voiceChannel.leave();
+    msg.channel.send("No music in queue, leaving channel.");
+  }
 }
